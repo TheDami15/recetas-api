33acca788ac0c33fccd96d2e3f18c6e8227bd04d
TheDami15 <thedami15@gmail.com>
2024-04-01 11:39:27 +0100

    Added Recipes Container

--- src/components/RecipesBody.js
+++ src/components/RecipesBody.js
@@ -7,24 +7,31 @@ const RecipesBody = () => {
   const { data, loading } = useFetch("https://apirecetes-50a9e4e6edb1.herokuapp.com/api/"); 
 
   return (
-    <div className='body'>
-      <div className='container'>
-          <h1>
-            <strong style={{ color: '#66cbbb' }}>Re</strong>
-            <strong style={{ color: '#f9b548' }}>ce</strong>
-            <strong style={{ color: '#ec2e1e' }}>tas</strong>
-          </h1>
-          
-          <ul className='rcpul'>
-          {loading && <li key="loading">Cargando...</li>}
-          {data?.map((recipes, index) => (
-            <li className='rcpli' key={recipes.id || index}>
-             <strong>Nombre de la receta:</strong> {recipes.nombre}. <br/><strong>Ingredientes:</strong> {recipes.ingredientes}
-            </li>
-          ))}
-        </ul>
+  <div className='body'>
+    <div className='container'>
+      <h1>
+        <strong style={{ color: '#66cbbb' }}>Re</strong>
+        <strong style={{ color: '#f9b548' }}>ce</strong>
+        <strong style={{ color: '#ec2e1e' }}>tas</strong>
+      </h1>
+      
+      <div className='recipes-container'>
+        {loading && <p key="loading">Cargando...</p>}
+        {data?.map((recipe, index) => (
+          <div className='recipe-card' key={recipe.id || index}>
+            <strong>Nombre de la receta:</strong> {recipe.nombre}. <br/>
+            <strong>Ingredientes:</strong>
+            <ul className='ingr'>
+              {recipe.ingredientes.split(',').map((ingredient, idx) => (
+                <li key={idx}>{ingredient.trim()}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
       </div>
     </div>
+  </div>
+
 
   )
 }
